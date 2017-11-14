62f4c5035591427ce7e516dd916e81f13ac67563
geoffreyhale <geoffreyhale@gmail.com>
2017-11-13 19:47:48 -0800

    server login details are for example only
    database no longer exists as such

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
 //connect.php
 
-$server = 'touch.geoffreyhale.com';
-$username = 'user_touch';
-$password = 'D+ubrUw5^';
-$database = 'geoffreyhale_touch';
+$server = 'touch.geoffreyhale.com'; //example
+$username = 'user_touch'; //example
+$password = 'D+ubrUw5^'; //example
+$database = 'geoffreyhale_touch'; //example
 
 if ( !mysql_connect( $server, $username, $password ) )
 {
